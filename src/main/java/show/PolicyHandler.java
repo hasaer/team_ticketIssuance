fc642f0760defcb497ea6543d4fd9801d50ee2c3
hasaer <hasaer@naver.com>
2020-06-13 18:41:05 +0900

    first commit

--- src/main/java/show/PolicyHandler.java
+++ src/main/java/show/PolicyHandler.java
@@ -11,11 +11,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class PolicyHandler{
     
+    @Autowired
+    TicketIssuanceRepository ticketIssuanceRepository;
+
     @StreamListener(KafkaProcessor.INPUT)
     public void wheneverPayed_IssueStatus(@Payload Payed payed){
 
         if(payed.isMe()){
-            System.out.println("##### listener IssueStatus : " + payed.toJson());
+            TicketIssuance ticketIssuance = new TicketIssuance();
+            ticketIssuance.setBookId(payed.getBookId());
+            ticketIssuance.setIssueStatus("Issuable");
+            ticketIssuanceRepository.save(ticketIssuance);
+            System.out.println("Your ticket is now " + ticketIssuance.getIssueStatus());
+            //System.out.println("##### listener IssueStatus : " + payed.toJson());
         }
     }
 
